947de7210b8b6ca8a19bb7e0b8722e831e181c41
saya3838 <86864442+saya3838@users.noreply.github.com>
2022-03-18 21:17:36 +0900

    ページ設定

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from blog.views import PostList
+from blog.views import PostList, PostDetail
 
 urlpatterns =[
   #path("", PostList.as_view(), name="post_list"),
   path("", PostList.as_view(), name="index"),
+  path("post/<int:pk>/", PostDetail.as_view(), name="post_detail")
+  #pkとはプライマリーキーのこと
 ]
